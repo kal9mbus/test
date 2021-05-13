f4b0164b2d20edf1403be9ea43bcce36e45fef8c
Nikolay <nik.shevchuk.adm@list.ru>
2021-05-13 16:31:56 +1100

    testing new features

--- 2.1/script.js
+++ 2.1/script.js
@@ -9,5 +9,5 @@ let person = {
 console.log(person.name)
 person.name = prompt("Как тебя зовут?", "");
 console.log(person.name);
-let age_ = confirm("Тебе есть 18???");
+let age_ = confirm("Тебе есть 18??");
 console.log(age_);
